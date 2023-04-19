f54093663a66787056ff189950ba9f46470cfd79
mabelov <belov.eagle@mail.ru>
2023-04-19 19:24:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import time
+import field
+import belovFleet
+
+root = Tk()
+canvas = Canvas(root, width=500, height=500)
+canvas.pack()
+canvas.create_rectangle(0, 0, 500, 500,
+                        outline='white', fill='white')
+field.drawField(canvas)
+field.placeShips()
+
+while not field.isGameOver():
+    coord = belovFleet.shoot()
+    x = coord[0]
+    y = coord[1]
+    kill = field.shoot(x, y, canvas)
+    belovFleet.observe(x, y, kill)
+    canvas.update()
+    time.sleep(0.5)
+
+field.showGameOver()
